c21dcee221658c817fa0fd5bf8d174be46fff1de
Marius Mathiesen <marius@shortcut.no>
2017-01-25 11:27:47 +0100

    Add namespace to elisp funs to avoid polluting globals

--- kata.el
+++ kata.el
@@ -1,23 +1,23 @@
 ;; Set a global counter of the number of failed test runs
-(setq failed-runs 0)
+(setq kata/failed-runs 0)
 
-(defun run-with-output (label result)
+(defun kata/run-with-output (label result)
   "Print a human-readable version of combining label and result. Increment the number of failed runs if result is incorrect"
   (message (concat label ": " (number-to-string result)))
-  (unless (= 21 result) (setq failed-runs (+ 1 failed-runs)))
+  (unless (= 21 result) (setq kata/failed-runs (+ 1 kata/failed-runs)))
   )
 
 
-(defun add-for (list count sum)
+(defun kata/add-for (list count sum)
   "Add to sum each number in list for count iterations"
   (if (= 0 count) sum
     (progn
       (setq sum (+ (car list) sum))
-      (add-for (cdr list) (- count 1) sum))))
+      (kata/add-for (cdr list) (- count 1) sum))))
 
-(run-with-output "add-for" (add-for '(1 2 3 4 5 6) 6 0))
+(kata/run-with-output "kata/add-for" (kata/add-for '(1 2 3 4 5 6) 6 0))
 
-(defun add-while (numbers)
+(defun kata/add-while (numbers)
   "Add each number in numbers using a while-loop, return the sum of numbers"
   (setq sum 0)
   (while numbers
@@ -25,20 +25,20 @@
     (setq numbers (cdr numbers)))
   sum)
 
-(run-with-output "add-while" (add-while '(1 2 3 4 5 6)))
+(kata/run-with-output "kata/add-while" (kata/add-while '(1 2 3 4 5 6)))
 
-(defun add-by-recursion (numbers sum)
+(defun kata/add-by-recursion (numbers sum)
   "Add each numbers in numbers to sum, then return"
   (if numbers
       (progn
         (setq sum (+ sum (car numbers)))
-        (add-by-recursion (cdr numbers) sum)
+        (kata/add-by-recursion (cdr numbers) sum)
         )
     sum))
 
-(run-with-output "add-by-recursion" (add-by-recursion '(1 2 3 4 5 6) 0))
+(kata/run-with-output "kata/add-by-recursion" (kata/add-by-recursion '(1 2 3 4 5 6) 0))
 
 ;; Exit Emacs with a non-zero status if there have been errors
-(unless (= 0 failed-runs)
+(unless (= 0 kata/failed-runs)
   (progn
     (error (kill-emacs 1))))
